1c03dd5439d638bd8845d012b84aa3615061bbd8
Edgar Morales <edmorales94@gmail.com>
2017-10-23 17:06:07 -0400

    Class that will allows to work with regular expressions

--- project/TokenData.java
+++ project/TokenData.java
@@ -8,15 +8,29 @@ public class TokenData {
 	private Pattern pattern;
 	private TokenType type;
 	
+/*******************************************************************************************************
+ * This constructor will allow the instance to have access to the Pattern class 
+ * which can work with regular expressions
+ * @param Pattern
+ * @param TokenType
+ */
 	public TokenData(Pattern pattern, TokenType type){
 		this.pattern = pattern;
 		this.type = type;
 	}
 	
+/*******************************************************************************************************
+ * Returns a regular expression pattern
+ * @return Pattern
+ */
 	public Pattern getPattern(){
 		return pattern;
 	}
 	
+/*******************************************************************************************************
+ * Returns the TokenType
+ * @return TokenType
+ */
 	public TokenType getType(){
 		return type;
 	}
