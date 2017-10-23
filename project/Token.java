cc400c2960eccce95c6a3161e5adf8323cf3ab5f
Edgar Morales <edmorales94@gmail.com>
2017-10-23 16:57:59 -0400

    This class parses the given input

--- project/Token.java
+++ project/Token.java
@@ -1,5 +1,4 @@
 package project;
-
 /**
  * @author Edgar Morales
  *
@@ -7,16 +6,28 @@ package project;
 public class Token {
 	private String token;
 	private TokenType type;
-	
+
+/*****************************************************************************************************************************************
+ * @param String
+ * @param TokenType
+ */
 	public Token(String token, TokenType type){
 		this.token = token;
 		this.type = type;
 	}
 	
+/*****************************************************************************************************************************************
+ * Returns a Token
+ * @return token
+ */
 	public String getToken(){
 		return token;
 	}
 	
+/*****************************************************************************************************************************************
+ * Returns the TokenType: token, identifier, string, integer, etc
+ * @return TokenType
+ */
 	public TokenType getType(){
 		return type;
 	}
