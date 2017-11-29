d8c59643bde74b60e824d75fc0f51a003b45d668
Edgar Morales <edmorales94@gmail.com>
2017-11-29 15:36:52 -0500

    Updated some variable types and added comments

--- ToyLanguage/ToyLanguage.java
+++ ToyLanguage/ToyLanguage.java
@@ -1,6 +1,7 @@
 package ToyLanguage;
 
 import java.util.ArrayList;
+//import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,42 +14,52 @@ import javax.script.ScriptException;
  *
  */
 public class ToyLanguage {
-	public static ArrayList<String> tokens = new ArrayList<String>();
-	public static Hashtable<String, String> symbolTable = new Hashtable<String, String>();
+	public static ArrayList<String> tokens = new ArrayList<String>();//Every character will get store here
+	public static Hashtable<String, String> symbolTable = new Hashtable<String, String>();//Variables and their values will be stored here
 	
-	public static ArrayList<String> list(String data) {
-		ArrayList<String> characters = new ArrayList<String>();
-		for(int i = 0; i < data.length(); i++) {
-			char character = data.charAt(i);
-			characters.add(String.valueOf(character));
+/*******************************************************************************************************************************************************************
+ * This method will read every single character from the input and store them in an ArrayList
+ * @param String data
+ * @return ArrayList characters
+ */
+	public static String[] list(String data) {
+		String[] characters = new String[data.length()];//Array needed for storing each character
+		for(int i = 0; i < data.length(); i++) { //Loop through the whole input
+			characters[i] = String.valueOf(data.charAt(i));//get the character at the specific location and cast it to string. Then add it to the array
 		}
+		//System.out.println(Arrays.toString(characters));
 		return characters;
 	}
 	
+/*******************************************************************************************************************************************************************
+ * This method converts a sequence of characters into a sequence of tokens (strings with an assigned and thus identified meaning).
+ * @param String assignment
+ * @return ArrayList of tokens
+ */
 	public static ArrayList<String> tokenizer(String assignment){
-		String token = "";
 		int state = 0; //found a keyword, variable, etc
 		String string = "";
-		ArrayList<String> data = list(assignment);
-		String numericalExpression = "";
-		boolean isAnExpression = false;
-		boolean possibleVariable = false;
-		String variableName = "";
+		String[] data = list(assignment);//get each character from assignament and store them in data[]
+		String token = "";//This will hold each character from the assignment
+		String numericalExpression = "";//Used to store mathetical expressions such as (347+[-12*3]-10)
+		boolean isAnExpression = false;//This will let us know if we have an expression or just a single number
+		boolean possibleVariable = false;//This will let us know if we are storing a variable name or not
+		String variableName = "";//each character belonging to a variable name will be store here
 		
-		for (int i = 0; i < data.size(); i++) {
-			token += data.get(i);
-			//System.out.print(token); // + " " 
-			if(token.equalsIgnoreCase(" ")) {
-				if(state == 0) { token = "";}
-				else { token = " ";}
+		for (int i = 0; i < data.length; i++) {//loop through each character
+			token += data[i];//update token by appending each character stored in data[]
+			//System.out.print(token);
+			if(token.equalsIgnoreCase(" ")) {//if we get a space
+				if(state == 0) { token = "";}//if it's not a space from a string, get rid of spaces
+				else { token = " ";}//if it belongs to a string value, we keep it
 			}
 			
-			else if(token.equalsIgnoreCase("\n") || token.equalsIgnoreCase(";")) {
-				if(isAnExpression && numericalExpression.equalsIgnoreCase("") == false) {
+			else if(token.equalsIgnoreCase("\n") || token.equalsIgnoreCase(";")) {//in case we find a new line or semicolon
+				if(isAnExpression && numericalExpression.equalsIgnoreCase("") == false) {//if we have a mathematical expression and numericalExpression is not empty
 					//System.out.print("Numerical Expression: " + numericalExpression);
-					tokens.add("Express: " + numericalExpression);
-					tokens.add("Semico: " + token);
-					numericalExpression = "";
+					tokens.add("Express: " + numericalExpression);//add the exact mathematical expression
+					tokens.add("Semico: " + token);//since token is a ; we added it after the expression
+					numericalExpression = "";//we clear the expression for a possible next new expression
 				}
 				else if(numericalExpression.equals("") == false && !(isAnExpression)) {
 					//System.out.print("Number: " + numericalExpression);
@@ -192,7 +203,7 @@ public class ToyLanguage {
 	
 	
 	public static void main(String[] args) throws ScriptException {
-		String data = "$myVariable = 345"
+		String data = "$myVariable = 345;"
 				+ "$myVarible2 = (10+2)*4;";
 		tokenizer(data);
 		System.out.println(evaluateExpression("---(1+2)*(1-+2)"));
